4f40d954bb5656d85804a5226462fb012c798806
zhangdl <zhangdl945@163.com>
2022-07-25 21:17:46 +0800

    first commit

---
+++ src/test/java/com/zhangdl/mygit/MygitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangdl.mygit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
